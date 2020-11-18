2d84ca8defd3483759a08278e4a4c01fa4d8df45
FrankRay78 <52075808+FrankRay78@users.noreply.github.com>
2020-11-18 11:58:35 +0000

    Removed OpenCoverToCoberturaConverter (no longer needed)

--- build.bat
+++ build.bat
@@ -11,7 +11,4 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 packages\OpenCover.4.7.922\tools\OpenCover.Console.exe -register:user -target:"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\Extensions\TestPlatform\vstest.console.exe" -targetargs:"Game-Of-Life_TESTS\bin\Debug\Game-Of-Life_TESTS.dll /TestAdapterPath:packages\MSTest.TestAdapter.1.3.2" -filter:"+[*]* -[*TESTS]*" -output:TestResults\OpenCover.XML -skipautoprops
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-REM packages\OpenCoverToCoberturaConverter.0.3.4\tools\OpenCoverToCoberturaConverter.exe -input:TestResults\coverage.xml -output:TestResults\Cobertura.xml
-REM if %errorlevel% neq 0 exit /b %errorlevel%
-
 packages\ReportGenerator.4.8.0\tools\net47\ReportGenerator.exe -reports:TestResults\OpenCover.xml -targetdir:TestResults\ReportGenerator -reportTypes:"HtmlInline;HtmlChart;Cobertura;" -historydir:"..\..\persistent\Game-Of-Life\cover"
